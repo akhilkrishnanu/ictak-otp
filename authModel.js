c6edd3bc378c93c633382fbaeee1cee9d99a7ff3
akhilkrishnanu <akhilku@gmail.com>
2022-08-28 16:56:13 +0530

    Update authModel.js

--- authModel.js
+++ authModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const DB = "mongodb+srv:"
+const DB = process.env.MONGO_URI
 mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
  console.log("Database Connection Successful")
 }).catch((err) => {
